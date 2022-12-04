363a4944c9a0bc178f16837dcf1904b418df83ef
awxkee <qnljoy@gmail.com>
2022-12-04 23:21:13 +0800

    built latest libpng

--- Package.swift
+++ Package.swift
@@ -16,8 +16,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "libpng",
             path: "Sources/libpng.xcframework")
